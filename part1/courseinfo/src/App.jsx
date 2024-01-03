eb7e8a6ec81d0fc46099bf9e3d79c6baef10a7d7
Maksym Blashko <blashkoma@gmail.com>
2024-01-03 21:18:23 +0200

    solution for part1 - 1.5 refactoring (step 5)

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,4 +1,4 @@
-const Header = (props) => <h1>{props.course}</h1>
+const Header = (props) => <h1>{props.courseName}</h1>
 const Total = (props) => <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
 const Part = (props) => <p>{props.part.name} {props.part.exercises}</p>
 const Content = (props) => {
@@ -12,8 +12,9 @@ const Content = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
+  const course = {
+  name: 'Half Stack application development',
+  parts: [
     {
       name: 'Fundamentals of React',
       exercises: 10
@@ -27,14 +28,13 @@ const App = () => {
       exercises: 14
     }
   ]
+}
 
   return (
     <div>
-      <Header course={course} />
-      {/* <Content part1={part1} part2={part2} part3={part3} />
-      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} /> */}
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
