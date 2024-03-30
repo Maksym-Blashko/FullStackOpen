5d0a1c621998e26b9f55ae92dd0591a37d4bb684
Maksym Blashko <blashkoma@gmail.com>
2024-03-30 07:33:56 +0200

    blog list expansions 4.17 (step 5)

--- part4/bloglist/controllers/blog_list.js
+++ part4/bloglist/controllers/blog_list.js
@@ -1,9 +1,10 @@
 const blogListRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogListRouter.get('/', async (request, response, next) => {
     try {
-        const blogs = await Blog.find({})
+        const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
         response.json(blogs)
     } catch (error) {
         next(error)
@@ -19,15 +20,27 @@ blogListRouter.post('/', async (request, response, next) => {
             return response.status(400).json({ error: 'Bad Request: Missing url' })
         }
 
+        let user
+        if (request.body.userId) {
+            user = await User.findById(request.body.userId)
+        } else {
+            user = await User.findOne({})
+        }
+
         const blog = new Blog({
             title: request.body.title,
             author: request.body.author,
             url: request.body.url,
-            likes: request.body.likes ? request.body.likes : 0
+            likes: request.body.likes ? request.body.likes : 0,
+            user: user.id
         })
 
-        const result = await blog.save()
-        response.status(201).json(result)
+        const savedBlog = await blog.save()
+
+        user.blogs = user.blogs.concat(savedBlog._id)
+        await user.save()
+
+        response.status(201).json(savedBlog)
     } catch (error) {
         next(error)
     }
