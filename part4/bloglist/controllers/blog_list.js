80c8312b3568aff96ea63cb784eb83b8756dffad
Maksym Blashko <blashkoma@gmail.com>
2024-03-31 18:04:51 +0300

    blog list expansions 4.20 (step 8)

--- part4/bloglist/controllers/blog_list.js
+++ part4/bloglist/controllers/blog_list.js
@@ -3,14 +3,6 @@ const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-const getTokenFrom = request => {
-    const authorization = request.get('authorization')
-    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-        return authorization.substring(7)
-    }
-    return null
-}
-
 blogListRouter.get('/', async (request, response, next) => {
     try {
         const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
@@ -22,12 +14,7 @@ blogListRouter.get('/', async (request, response, next) => {
 
 blogListRouter.post('/', async (request, response, next) => {
     try {
-        const token = getTokenFrom(request)
-        if (!token) {
-            return response.status(401).json({ error: 'Unauthorized: Missing token' })
-        }
-
-        const decodedToken = jwt.verify(token, process.env.SECRET)
+        const decodedToken = jwt.verify(request.token, process.env.SECRET)
         if (!decodedToken.id) {
             return response.status(401).json({ error: 'Unauthorized: Invalid token' })
         }
